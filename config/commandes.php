74eeca6986eff00ad3fca5f4708915c546f4efaa
hkfmz <hyperpro200497@gmail.com>
2022-05-19 17:12:32 +0200

    les fonctions

--- config/commandes.php
+++ config/commandes.php
@@ -1,11 +1,83 @@
 <?php
 
+function ajouterUser($nom, $prenom, $email, $motdepasse)
+{
+  if(require("connexion.php"))
+  {
+    $req = $access->prepare("INSERT INTO utilisateurs (nom, prenom, email, motdepasse) VALUES (?, ?, ?, ?)");
+
+    $req->execute(array($nom, $prenom, $email, $motdepasse));
+
+    return true;
+
+    $req->closeCursor();
+  }
+}
+
+// function getUsers($email, $password){
+  
+//   if(require("connexion.php")){
+
+//     $req = $access->prepare("SELECT * FROM utilisateur ");
+
+//     $req->execute();
+
+//     if($req->rowCount() == 1){
+      
+//       $data = $req->fetchAll(PDO::FETCH_OBJ);
+
+//       foreach($data as $i){
+//         $mail = $i->email;
+//         $mdp = $i->motdepasse;
+//       }
+
+//       if($mail == $email AND $mdp == $password)
+//       {
+//         return $data;
+//       }
+//       else{
+//           return false;
+//       }
+
+//     }
+
+//   }
+
+// }
+
+function modifier($image, $nom, $prix, $desc, $id)
+{
+  if(require("connexion.php"))
+  {
+    $req = $access->prepare("UPDATE produits SET `image` = ?, nom = ?, prix = ?, description = ? WHERE id=?");
+
+    $req->execute(array($image, $nom, $prix, $desc, $id));
+
+    $req->closeCursor();
+  }
+}
+
+function afficherUnProduit($id)
+{
+	if(require("connexion.php"))
+	{
+		$req=$access->prepare("SELECT * FROM produits WHERE id=?");
+
+        $req->execute(array($id));
+
+        $data = $req->fetchAll(PDO::FETCH_OBJ);
+
+        return $data;
+
+        $req->closeCursor();
+	}
+}
 
   function ajouter($image, $nom, $prix, $desc)
   {
     if(require("connexion.php"))
     {
-      $req = $access->prepare("INSERT INTO produits (image, nom, prix, description) VALUES ('$image', '$nom', $prix, '$desc')");
+      $req = $access->prepare("INSERT INTO produits (image, nom, prix, description) VALUES (?, ?, ?, ?)");
 
       $req->execute(array($image, $nom, $prix, $desc));
 
